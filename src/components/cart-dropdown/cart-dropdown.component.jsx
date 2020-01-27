31add8209f8ebc3bba0f561a12131d43638b33e9
Oni Benjamin <onibenjo@gmail.com>
2020-01-27 17:19:36 +0100

    Refactored cart selectors with reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "./../../redux/cart/cart.selectors";
 
 const CardDropdown = () => {
-  const cartItems = useSelector(state => state.cart.cartItems);
+  const cartItems = useSelector(state => selectCartItems(state));
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" >
-        {
-          cartItems.map(cartItem => (
-            <CartItem key={cartItem.id} item={cartItem} />
-          ))
-        }
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
       </div>
       <CustomButton>CHECKOUT</CustomButton>
     </div>
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,12 +4,11 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleCartHidden } from "./../../redux/cart/cart.actions";
+import { selectCartItemsCount } from './../../redux/cart/cart.selectors';
 
 const CartIcon = () => {
   const dispatch = useDispatch();
-  const itemCount = useSelector(state =>
-    state.cart.cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
-  );
+  const itemCount = useSelector(state => selectCartItemsCount(state));
   return (
     <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
       <ShoppingIcon className="shopping-icon" />
