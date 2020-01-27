e246f98af2a424f348c7f12fb0c42f255c4a1ff5
Oni Benjamin <onibenjo@gmail.com>
2020-01-27 16:48:40 +0100

    Added the itemcount functionality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleCartHidden } from "./../../redux/cart/cart.actions";
 
 const CartIcon = () => {
   const dispatch = useDispatch();
+  const itemCount = useSelector(state =>
+    state.cart.cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+  );
   return (
-    <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}> 
+    <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
