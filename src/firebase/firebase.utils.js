2534a64f977c395c3799fb8072b1b1fb0f5b172f
Oni Benjamin <onibenjo@gmail.com>
2020-01-29 10:57:15 +0100

    Updated the db config files

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,65 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// const config = {
+//   apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+//   authDomain: 'crwn-db.firebaseapp.com',
+//   databaseURL: 'https://crwn-db.firebaseio.com',
+//   projectId: 'crwn-db',
+//   storageBucket: 'crwn-db.appspot.com',
+//   messagingSenderId: '850995411664',
+//   appId: '1:850995411664:web:7ddc01d597846f65'
+// };
+const config = {
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_APPNAME}.firebaseapp.com`,
+  databaseURL: `https://${process.env.REACT_APP_APPNAME}.firebaseio.com`,
+  projectId: `${process.env.REACT_APP_APPNAME}`,
+  storageBucket: `${process.env.REACT_APP_APPNAME}.appspot.com`,
+  messagingSenderId: `${process.env.REACT_APP_MSG_ID}`,
+  appId: `${process.env.REACT_APP_APPID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
+};
+
+console.log(process.env);
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () =>
+  auth.signInWithPopup(provider).catch(err => {
+    console.log(err);
+    return;
+  });
